6e879dd225a754b1ee87af3dc9bb4fd29e667d73
paulo <paulomantillaa@gmail.com>
2026-02-22 20:42:13 -0600

    Initial Commit

---
+++ bankCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bankCard
+//
+//  Created by Paulo Mantilla on 22/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bankCard/bankCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  bankCardApp.swift
+//  bankCard
+//
+//  Created by Paulo Mantilla on 22/02/26.
+//
+
+import SwiftUI
+
+@main
+struct bankCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
