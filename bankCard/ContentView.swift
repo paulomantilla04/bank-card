11212a625333b4860a47ed6a7565b554e60e9733
paulo <paulomantillaa@gmail.com>
2026-02-22 20:47:03 -0600

    initial commit

--- bankCard/ContentView.swift
+++ bankCard/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack(){
+         Text("HOla")
         }
-        .padding()
+        .frame(width: 380, height: 350)
+        .background(.blue)
     }
 }
 
